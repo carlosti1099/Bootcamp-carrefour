7ec5ca32fe281e2a7332f14750380cf2e4ce4ba9
carlosti1099 <carlos.ti1099@gmail.com>
2022-05-02 11:02:29 -0400

    Criando uma api usando herokuapp

--- projeto-covid-19/src/containers/Main/index.jsx
+++ projeto-covid-19/src/containers/Main/index.jsx
@@ -1,6 +1,18 @@
-import React, { memo } from "react"
+import React, { memo, useCallback, useEffect, useState } from 'react'
+import Api from '../../api'
 
 function Main() {
+    const [data, setData] = useState({})
+    const [country, setCountry] = useState('brazil')
+
+    const getCovidData = useCallback((country) => {
+        Api.getCountry(country)
+            .then(data => setData(data))
+    }, [])
+
+    useEffect(() => {    
+        getCovidData(country)
+    },   [getCovidData, country])
 
     return (
         <div>
