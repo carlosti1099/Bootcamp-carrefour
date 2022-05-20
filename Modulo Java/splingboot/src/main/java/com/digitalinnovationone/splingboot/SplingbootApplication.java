7f527171df499fbe62713a11a7c9e9c7c474dc1b
carlosti1099 <carlos.ti1099@gmail.com>
2022-05-19 20:16:44 -0400

    Primeiro Hello, Digital Innovation One!

---
+++ Modulo Java/splingboot/src/main/java/com/digitalinnovationone/splingboot/SplingbootApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.splingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplingbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplingbootApplication.class, args);
+	}
+
+}
---
+++ Modulo Java/splingboot/src/test/java/com/digitalinnovationone/splingboot/SplingbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovationone.splingboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplingbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
